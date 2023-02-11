941ac48a7600eaff4a75064382ca221c1b0dcaf9
Ivan Morozko <Ivan.Morozko@oktetlabs.ru>
2023-02-11 23:57:15 +0400

    fix bug: forgot to call std::dec after std::hex in IP parser

--- include/l3_parser.h
+++ include/l3_parser.h
@@ -63,15 +63,16 @@ class Ip : public Protocol<IpHeader, IpHeader::name> {
     std::cout << "  ECN: " << (header.dscp_and_ecn & 0x03) << '\n';
     std::cout << "  Total length: " << header.total_length << " bytes" << '\n';
     std::cout << "  Identification: 0x" << std::hex << std::setfill('0')
-              << std::setw(4) << header.identification << std::dec << '\n';
+              << std::setw(4) << header.identification << '\n';
     std::cout << "  Flags: "
-              << ((header.flags_and_fragment_offset >> 13) & 0x07) << '\n';
+              << ((header.flags_and_fragment_offset >> 13) & 0x07) << std::dec
+              << '\n';
     std::cout << "  Fragment offset: "
               << (header.flags_and_fragment_offset & 0x1FFF) << '\n';
     std::cout << " TTL: " << static_cast<int>(header.ttl) << '\n';
     std::cout << " Protocol: " << static_cast<int>(header.protocol) << '\n';
     std::cout << " Header checksum: 0x" << std::hex << std::setfill('0')
-              << std::setw(4) << header.header_checksum << '\n';
+              << std::setw(4) << header.header_checksum << std::dec << '\n';
 
     char source_address_str[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &header.source_address, source_address_str,
